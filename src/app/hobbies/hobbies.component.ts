7b441b565de3fd6d4c3f9b7ce0860cced2549a0d
Daniel Hjertholm <danhje@gmail.com>
2019-08-28 23:42:41 +0200

    Gave hobby components absolute position

--- src/app/hobbies/hobbies.component.ts
+++ src/app/hobbies/hobbies.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hobbies',
-  templateUrl: './hobbies.component.html'
+  templateUrl: './hobbies.component.html',
+  styleUrls: ['./hobbies.component.css']
 })
 export class HobbiesComponent {
 
