27ce084b3c82556d9ae7ab8e88ad62f5fdcda1e0
Daniel Hjertholm <danhje@gmail.com>
2019-09-05 15:49:00 +0200

    Removed the old Hobbies component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NgCircleProgressModule } from './hobby';
-import { HobbiesComponent } from './hobbies/hobbies.component';
 import { HobbiesTwoComponent } from './hobbies-2/hobbies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HobbiesComponent,
     HobbiesTwoComponent
   ],
   imports: [
     BrowserModule,
-    NgCircleProgressModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
