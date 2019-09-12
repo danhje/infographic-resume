89c520c2ef1bd96e9f49bdc8e782f895159c5d92
Daniel Hjertholm <danhje@gmail.com>
2019-09-12 21:11:08 +0200

    Popover animates backwards as well as forwards

--- src/app/popover/popover.component.ts
+++ src/app/popover/popover.component.ts
@@ -1,10 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-popover',
   templateUrl: './popover.component.html',
   styleUrls: ['./popover.component.css']
 })
-export class PopoverComponent {
-  @Input() popoverColor = 'red';
+export class PopoverComponent implements OnInit {
+  @Input() popoverColor = '';
+  visible = true;
+  shouldIgnoreClickout = true;
+  timer;
+
+  constructor(private eRef: ElementRef) {
+  }
+
+  startTimer() {
+    this.timer = setInterval(() => {
+      this.shouldIgnoreClickout = false;
+      clearInterval(this.timer);
+    }, 100);
+  }
+
+  ngOnInit() {
+    this.startTimer();
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+    if (this.eRef.nativeElement.contains(event.target)) {
+      console.log('clicked inside');
+    } else {
+      console.log('clicked outside');
+      if (!this.shouldIgnoreClickout) {
+        this.visible = false;
+      }
+    }
+  }
+
+
 }
