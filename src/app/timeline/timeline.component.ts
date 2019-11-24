08745c855ba4d40100639062f688cde23df8a408
Daniel Hjertholm <danhje@gmail.com>
2019-11-24 18:08:58 +0100

    Add skeleton Testimonial component

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -267,7 +267,7 @@ export class TimelineComponent implements OnInit {
         true,
         'Special Consultant at Sykehuspartner HF',
         'January 2014 - November 2019',
-        '2014 - 2018: IT Operations Manager at the Operations Bridge. Responsible for event management, as well as leading the resolution of major incidents by managing the team of technical experts that are mobilized to resolve the issue.<br /><br />2018 - present: Developer and team leader for the team that develops infrastructure monitoring and end user monitoring (EUM) through scripts that emulate users of our IT services.',
+        '2014 - 2018: IT Operations Manager at the Operations Bridge. Responsible for event management, as well as leading the resolution of major incidents by managing the team of technical experts that are mobilized to resolve the issue.<br /><br />2018 - present: Developer in the monitoring department, developing both infrastructure monitoring and end user monitoring (EUM) through scripts that emulate users of our IT services.',
         '2014.01',
         '2019.11'
       ),
